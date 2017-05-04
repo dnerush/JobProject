4b0e18017170d05c6c72dc24bd9e8f7b5bba77fe
Dima <2008Nerush2008@ukr.net>
2017-05-04 21:12:46 +0300

    Настроен парсер

---
+++ finalYearProject/src/main/java/com/work/Addition/FoundVacancy.java
@@ -0,0 +1,7 @@
+package com.work.Addition;
+
+/**
+ * Created by Sky_el on 04.05.2017.
+ */
+public class FoundVacancy {
+}
---
+++ finalYearProject/src/main/java/com/work/Addition/Parser.java
@@ -0,0 +1,7 @@
+package com.work.Addition;
+
+/**
+ * Created by Sky_el on 02.05.2017.
+ */
+public class Parser {
+}
